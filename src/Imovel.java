82663c437547a673e5406b77621186540e579fcb
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2016-10-02 15:39:00 -0300

    adsdas

---
+++ src/Imovel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leona_000
+ */
+public abstract class Imovel {
+    
+    protected int codigo, numero;
+    protected String logradouro, bairro, cidade, descrição;
+    protected double area_total, valor;
+    
+    
+}
