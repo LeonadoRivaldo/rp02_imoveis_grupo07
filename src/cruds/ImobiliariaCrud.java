cee7ab1d8cba15127d6441979e10291599cd1a63
clever oliveira <cleroliverjunior@gmail.com>
2016-10-03 14:17:28 -0300

    Interface Lista imoveis dentro de pacote Cruds

--- src/cruds/ImobiliariaCrud.java
+++ src/cruds/ImobiliariaCrud.java
@@ -8,6 +8,7 @@ package cruds;
 import imovel.Imovel;
 import java.util.ArrayList;
 import java.util.List;
+import persistenciaDados.ObjectCrud;
 
 /**
  *
@@ -16,5 +17,27 @@ import java.util.List;
 public class ImobiliariaCrud implements ListaImoveis{
     private List<Imovel> listaImoveis = new ArrayList();
     private String tipoImovel;
-    
+
+    public List<Imovel> getListaImoveis() {
+        return listaImoveis;
+    }
+
+    public String getTipoImovel() {
+        return tipoImovel;
+    }
+
+    public void setTipoImovel(String tipoImovel) {
+        this.tipoImovel = tipoImovel;
+    }
+    /**
+     * metodo que inclui um objeto na lista geral de imoveis
+     * @param imovel que foi criado na interface
+     * @return True caso inclua com sucesso, falso caso contrário
+     */
+    @Override
+    public boolean incluir(Imovel imovel) {
+        listaImovel.add(imovel);
+        ObjectCrud.saveObject(imovel, this.tipoImovel);
+        return true;
+    }
 }
